5794d4cab2a49285018f99d168c00a43c9cadc32
Dany M <dannymanastireanu@gmail.com>
2024-07-30 02:19:40 -0400

    Working with buffer

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#define BUFFER_DEFAULT_SIZE 1
+
+typedef struct serialized_buffer {
+    char *starting_address;
+    int size;
+    int next;
+} serialized_buffer;
 
 typedef struct dddd {
     char name[30];
@@ -8,20 +18,33 @@ typedef struct dddd {
 
 #define get_offset(struct, field) (unsigned int)(&(((struct *)0)->field))
 
+void init_serialized_buffer(serialized_buffer *buff) {
+    buff->starting_address = calloc(1, BUFFER_DEFAULT_SIZE);
+    buff->size = BUFFER_DEFAULT_SIZE;
+    buff->next = 0;
+}
+
+void serialize_date(serialized_buffer *buff, char *data, int nbytes) {
+    // Have space
+    printf("We have %d/%d bytes free space, and size of data to be copied is %d bytes \n", (buff->size - buff->next), buff->size, nbytes);
+    if(buff->size - buff->next < nbytes) {
+        printf("Allocate more space.");
+        uint32_t required_size = 0;
+        // while()
+        // TODO compute required data
+        buff->starting_address = (char *)realloc(buff->starting_address, buff->size + BUFFER_DEFAULT_SIZE);
+    } 
+
+    memcpy(buff->starting_address + buff->next, data, nbytes);
+    buff->next += nbytes;
+
+}
+
 int main() {
-    my_struct a;
-    a.age = 1;
-    a.weight = 2;
-
-    printf("Direct access to age: %d\n", a.age);
-
-    my_struct *b = &a;
-
-    printf("Direct access to age using pointer: %d\n", b->age);
-
-    printf("Direct access to age using pointer: %d\n", ((char *)b + (32)));
-    printf("Offest of age in the struct: %d\n", get_offset(my_struct, age));
-    printf("size of struct %d \n", sizeof(my_struct));
-    printf("hello\n");
+    serialized_buffer *sbuff = (serialized_buffer *)calloc(1, sizeof(serialized_buffer));
+    init_serialized_buffer(sbuff);
+    sbuff->starting_address[0] = 'A';
+    sbuff->next += 1;
+    serialize_date(sbuff, "B", 1);
     return 0;
 }
